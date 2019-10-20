46eebe24f1c27d253cdabd4ce28bbe87ff04dc65
Luu Duc Trung <luuductrung1234@gmail.com>
2019-10-20 15:45:28 +0700

    implement custom aspnetcore middleware to serve node_modules

---
+++ Test/LDTSolutions.Common.WebApi.Test/NodeModulesTest/TestStartup.cs
@@ -0,0 +1,15 @@
+﻿namespace LDTSolutions.Common.WebApi.Tests.NodeModulesTest
+{
+   using System;
+   using Microsoft.AspNetCore.Builder;
+
+   using LDTSolutions.Common.WebApi.NodeModules;
+
+   public class TestStartup
+   {
+      public void Configure(IApplicationBuilder app)
+      {
+         app.UseNodeModules(TimeSpan.FromSeconds(600));
+      }
+   }
+}
---
+++ Test/LDTSolutions.Common.WebApi.Test/NodeModulesTest/UseNodeModulesIntegrationTest.cs
@@ -0,0 +1,60 @@
+﻿namespace LDTSolutions.Common.WebApi.Tests.NodeModulesTest
+{
+   using System;
+   using System.IO;
+   using System.Net;
+
+   using Microsoft.AspNetCore.Hosting;
+   using Microsoft.AspNetCore.TestHost;
+
+   using Xunit;
+
+   public class UseNodeModulesIntegrationTest
+   {
+      [Fact]
+      public async void RespondsToFileInNodeModules()
+      {
+         var builder = new WebHostBuilder();
+         builder.UseContentRoot(Directory.GetCurrentDirectory());
+         builder.UseStartup<TestStartup>();
+         var server = new TestServer(builder);
+         var client = server.CreateClient();
+
+         var result = await client.GetAsync("/node_modules/hello.txt");
+
+         Assert.Equal(TimeSpan.FromSeconds(600), result.Headers.CacheControl.MaxAge);
+         Assert.Equal("hello!", await result.Content.ReadAsStringAsync());
+      }
+
+      [Fact]
+      public async void IgnoresOutOfNodeModules()
+      {
+         var builder = new WebHostBuilder();
+         builder.UseContentRoot(Directory.GetCurrentDirectory());
+         builder.UseStartup<TestStartup>();
+         var server = new TestServer(builder);
+         var client = server.CreateClient();
+
+         var result = await client.GetAsync("hello.txt");
+
+         Assert.Equal(HttpStatusCode.NotFound, result.StatusCode);
+      }
+
+      [Fact]
+      public async void RespondsToFileInRequestPath()
+      {
+         var builder = new WebHostBuilder();
+         builder.UseContentRoot(Directory.GetCurrentDirectory());
+         builder.UseStartup<RequestPathStartup>();
+         var server = new TestServer(builder);
+         var client = server.CreateClient();
+
+         var result = await client.GetAsync("/vendor/hello.txt");
+
+         Assert.Equal(TimeSpan.FromSeconds(600), result.Headers.CacheControl.MaxAge);
+         Assert.Equal("hello!", await result.Content.ReadAsStringAsync());
+      }
+
+
+   }
+}
