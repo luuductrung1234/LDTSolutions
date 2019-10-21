648067b59bc5755b30b26c20957a485cda716cf7
Luu Duc Trung <luuductrung1234@gmail.com>
2019-10-21 23:34:27 +0700

    setup EntityFrameworkCore to infrastructure layer

---
+++ Services/WebApps/OdeToFood.Data/DataContext/OdeToFoodDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using OdeToFood.Core.Models;
+
+namespace OdeToFood.Data.DataContext
+{
+   public class OdeToFoodDbContext : DbContext
+   {
+      public DbSet<Restaurant> Restaurants { get; set; }
+   }
+}
