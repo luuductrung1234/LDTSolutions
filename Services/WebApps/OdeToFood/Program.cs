c3023ebdb6891fb60dbf7577b1acab9af920ed93
Luu Duc Trung <luuductrung1234@gmail.com>
2019-11-10 21:17:44 +0700

    update

--- Services/WebApps/OdeToFood/Program.cs
+++ Services/WebApps/OdeToFood/Program.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System.IO;
+using System.Reflection;
+
+using OdeToFood.Data.DataContext;
 
 namespace OdeToFood
 {
@@ -14,7 +13,24 @@ namespace OdeToFood
    {
       public static void Main(string[] args)
       {
-         CreateWebHostBuilder(args).Build().Run();
+         IWebHost host = CreateWebHostBuilder(args).Build();
+
+         var path = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+         Directory.SetCurrentDirectory(path);
+
+         AutoMigrateData(host);
+
+         host.Run();
+      }
+
+      private static void AutoMigrateData(IWebHost host)
+      {
+         using (var scope = host.Services.CreateScope())
+         {
+            var dbContext = scope.ServiceProvider.GetRequiredService<OdeToFoodDbContext>();
+
+            dbContext.Database.Migrate();
+         }
       }
 
       public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
--- Services/WebApps/OdeToFood/Startup.cs
+++ Services/WebApps/OdeToFood/Startup.cs
@@ -1,32 +1,39 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using LDTSolutions.Common.WebApi.NodeModules;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
+using LDTSolutions.Common.WebApi.NodeModules;
 using OdeToFood.Data.Compositions;
 
 namespace OdeToFood
 {
    public class Startup
    {
-      public Startup(IConfiguration configuration)
+      public Startup(IConfiguration configuration, IHostingEnvironment env)
       {
          Configuration = configuration;
+         Environment = env;
       }
 
       public IConfiguration Configuration { get; }
+      public IHostingEnvironment Environment { get; }
 
       // This method gets called by the runtime. Use this method to add services to the container.
       public void ConfigureServices(IServiceCollection services)
       {
+         using (var scope = (new DefaultServiceProviderFactory()
+                                 .CreateServiceProvider(services))
+                                 .CreateScope())
+         {
+            var logger = scope.ServiceProvider.GetRequiredService<ILogger<Startup>>();
+            logger.LogInformation($"Application Service hosted in {Environment.EnvironmentName} environment");
+            logger.LogInformation($"Web root path: {Environment.WebRootPath}");
+         }
+
          services.Configure<CookiePolicyOptions>(options =>
          {
             // This lambda determines whether user consent for non-essential cookies is needed for a given request.
