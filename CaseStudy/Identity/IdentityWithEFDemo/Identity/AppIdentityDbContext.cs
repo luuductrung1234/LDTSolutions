b25da2edda10ad1440bc8137b2c22567433afe76
Luu Duc Trung <luuductrung1234@gmail.com>
2019-11-14 17:16:07 +0700

    update

---
+++ CaseStudy/Identity/IdentityWithEFDemo/Identity/AppIdentityDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityWithEFDemo.Identity
+{
+   public class AppIdentityDbContext : IdentityDbContext<ApplicationUser, ApplicationRole, Guid>
+   {
+      public AppIdentityDbContext(DbContextOptions<AppIdentityDbContext> options) : base(options)
+      {
+
+      }
+   }
+}
---
+++ CaseStudy/Identity/IdentityWithEFDemo/Identity/ApplicationRole.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity;
+
+namespace IdentityWithEFDemo.Identity
+{
+   public class ApplicationRole : IdentityRole<Guid>
+   {
+   }
+}
