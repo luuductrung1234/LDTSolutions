0f762f47d3928be12567a4253b76386b8f39700e
Luu Duc Trung <luuductrung1234@gmail.com>
2019-11-24 15:40:31 +0700

    refactor code

---
+++ CaseStudy/Identity/SecureUserManagementDemo/Identity/IdentityServiceCollectionExtension.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+
+using IdentityDemo.Infrastructure.Identity;
+
+using LDTSolutions.Common.Identity;
+
+namespace SecureUserManagementDemo.Identity
+{
+   public static class IdentityServiceCollectionExtension
+   {
+      public static IServiceCollection AddCustomIdentity(this IServiceCollection services)
+      {
+         //var connectionString = "Server=TRUNG-LUU\\TRUNGSQLSERVER;Database=PluralsightDemo;User ID=sa;Password=Trung1997;";
+         var connectionString = "Server=localhost,1433;Database=PluralsightDemo;User ID=sa;Password=Trung1997;";
+         var migrationAssembly = typeof(AppIdentityDbContext).Assembly.GetName().Name;
+         services.AddDbContext<AppIdentityDbContext>(options =>
+            options.UseSqlServer(connectionString, opts => opts.MigrationsAssembly(migrationAssembly)));
+
+         // regist BCryptPasswordHasher before the call AddIdentity to skip the default implementation, PasswordHasher<>
+         services.AddScoped<IPasswordHasher<ApplicationUser>, BCryptPasswordHasher<ApplicationUser>>();
+
+         services.AddIdentity<ApplicationUser, ApplicationRole>(options =>
+            {
+               options.Tokens.EmailConfirmationTokenProvider = CustomIdentityOptions.EmailConfirmationTokenProvider;
+            })
+            .AddEntityFrameworkStores<AppIdentityDbContext>()
+            .AddUserStore<UserStore<ApplicationUser, ApplicationRole, AppIdentityDbContext, Guid>>()
+            .AddRoleStore<RoleStore<ApplicationRole, AppIdentityDbContext, Guid>>()
+            .AddClaimsPrincipalFactory<CustomUserClaimsPrincipalFactory>()
+            .AddDefaultTokenProviders()
+            .AddTokenProvider<EmailConfirmationTokenProvider<ApplicationUser>>(CustomIdentityOptions.EmailConfirmationTokenProvider);
+
+         services.Configure<DataProtectionTokenProviderOptions>(options =>
+            options.TokenLifespan = TimeSpan.FromHours(1));
+
+         services.Configure<EmailConfirmationTokenProviderOptions>(options =>
+            options.TokenLifespan = TimeSpan.FromDays(2));
+
+         services.ConfigureApplicationCookie(options => options.LoginPath = "/Auth/Login");
+
+         return services;
+      }
+   }
+}
--- CaseStudy/Identity/SecureUserManagementDemo/Startup.cs
+++ CaseStudy/Identity/SecureUserManagementDemo/Startup.cs
@@ -2,14 +2,9 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.AspNetCore.Identity;
-
-using IdentityDemo.Infrastructure.Identity;
 
 using SecureUserManagementDemo.Identity;
 
@@ -37,30 +32,7 @@ namespace SecureUserManagementDemo
 
          services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
-         //var connectionString = "Server=TRUNG-LUU\\TRUNGSQLSERVER;Database=PluralsightDemo;User ID=sa;Password=Trung1997;";
-         var connectionString = "Server=localhost,1433;Database=PluralsightDemo;User ID=sa;Password=Trung1997;";
-         var migrationAssembly = typeof(AppIdentityDbContext).Assembly.GetName().Name;
-         services.AddDbContext<AppIdentityDbContext>(options =>
-            options.UseSqlServer(connectionString, opts => opts.MigrationsAssembly(migrationAssembly)));
-
-         services.AddIdentity<ApplicationUser, ApplicationRole>(options =>
-            {
-               options.Tokens.EmailConfirmationTokenProvider = CustomIdentityOptions.EmailConfirmationTokenProvider;
-            })
-            .AddEntityFrameworkStores<AppIdentityDbContext>()
-            .AddUserStore<UserStore<ApplicationUser, ApplicationRole, AppIdentityDbContext, Guid>>()
-            .AddRoleStore<RoleStore<ApplicationRole, AppIdentityDbContext, Guid>>()
-            .AddClaimsPrincipalFactory<CustomUserClaimsPrincipalFactory>()
-            .AddDefaultTokenProviders()
-            .AddTokenProvider<EmailConfirmationTokenProvider<ApplicationUser>>(CustomIdentityOptions.EmailConfirmationTokenProvider);
-
-         services.Configure<DataProtectionTokenProviderOptions>(options =>
-            options.TokenLifespan = TimeSpan.FromHours(1));
-
-         services.Configure<EmailConfirmationTokenProviderOptions>(options =>
-            options.TokenLifespan = TimeSpan.FromDays(2));
-
-         services.ConfigureApplicationCookie(options => options.LoginPath = "/Auth/Login");
+         services.AddCustomIdentity();
       }
 
       // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -79,6 +51,7 @@ namespace SecureUserManagementDemo
 
          app.UseHttpsRedirection();
 
+         // add this middleware to enable authentation functionality
          app.UseAuthentication();
 
          app.UseStaticFiles();
